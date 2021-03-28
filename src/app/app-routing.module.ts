4c271fc4188546de49807f8e0873880c665fae69
Harrison Fritz <windingroad100hf@gmail.com>
2021-03-28 03:33:23 +0100

    routing, homepage, toolbar, subpages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { PurchasesComponent } from './purchases/purchases.component'
+import { ScoreComponent } from './score/score.component'
+import { CharitiesComponent } from './charities/charities.component'
+const routes: Routes = [{path:'home', component:HomeComponent},{path:'home', component:HomeComponent},{path:'purchases', component:PurchasesComponent},{path:'score', component:ScoreComponent},{path:'charities', component:CharitiesComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+import { PurchasesComponent } from './purchases/purchases.component';
+import { ScoreComponent } from './score/score.component';
+import { CharitiesComponent } from './charities/charities.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PurchasesComponent,
+    ScoreComponent,
+    CharitiesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
