9c4a61b2125f579ebf812c5eb5a1f2feb0794344
Harrison Fritz <windingroad100hf@gmail.com>
2021-04-04 03:57:59 +0100

    added initial get request

--- node/app.js
+++ node/app.js
@@ -5,6 +5,12 @@ const router = new KoaRouter();
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
-app.use(router.routes()).use(router.allowedMethods());
-app.use('/purchases', require('./microservices/purchases/purchases.router'));
+// app.use(router.routes()).use(router.allowedMethods());
+// app.use('/purchases', require('./microservices/purchases/purchases.router'));
+router.get('/test', testfunction);
+
+function testfunction(){
+  this.body= "Test Successful";
+
+}
 app.listen(3000);
